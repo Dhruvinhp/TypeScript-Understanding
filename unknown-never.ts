7024ce5c6de5035e2fba9a383d28a4df8f9fd4cd
dhruvinhp <dhruvinhprajapati@gmail.com>
2022-07-04 14:37:14 +0530

    feat: unknown never types

--- unknown-never.ts
+++ unknown-never.ts
@@ -1,14 +1,22 @@
 // ====== Unknown Type ======
-var userInput;
-var userName;
+
+let userInput: unknown;
+let userName: string;
+
 userInput = 5;
 userInput = "Max";
+
 // username = userInput // ERROR - Type 'unknown' is not assignable to type 'string', cause we are not sure what type of data is in userInput
+
 if (typeof userInput === "string") {
-    userName = userInput; // OK - userInput is a string for sure
+  userName = userInput; // OK - userInput is a string for sure
 }
+
 // ====== Never Return Type ======
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
+
+function generateError(message: string, code: number): never {
+  // here we're sure that this function will never return
+  throw { message: message, errorCode: code };
 }
+
 generateError("An error occurred!", 500);
